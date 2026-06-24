3a4303122e545e83ed4e730c23997c54bbda4499
Davziinn <mdavi6085@gmail.com>
2026-06-23 21:27:25 -0300

    iniciando a implementação do serviço de pacientes, criando apenas a conexção com o banco de dados e o container docker

---
+++ Servico-Pacientes/src/main/java/com/HealthSys/Servico_Pacientes/ServicoPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.HealthSys.Servico_Pacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoPacientesApplication.class, args);
+	}
+
+}
---
+++ Servico-Pacientes/src/test/java/com/HealthSys/Servico_Pacientes/ServicoPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HealthSys.Servico_Pacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
