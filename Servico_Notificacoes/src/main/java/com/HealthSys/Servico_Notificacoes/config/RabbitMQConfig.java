10ac0febb1151fb89f62f995c79055809cb0d4da
Davziinn <mdavi6085@gmail.com>
2026-08-02 09:38:37 -0300

    implementando a DLQ e o Ack MANUAL

--- Servico_Notificacoes/src/main/java/com/HealthSys/Servico_Notificacoes/config/RabbitMQConfig.java
+++ Servico_Notificacoes/src/main/java/com/HealthSys/Servico_Notificacoes/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.HealthSys.Servico_Notificacoes.config;
 
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
@@ -12,10 +14,32 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     public static final String QUEUE_CONSULTA_CRIADA = "notificacoes.consulta.criada";
+    public static final String DLX_NOTIFICACOES = "healthsys.notificacoes.dlx";
+    public static final String DLQ_CONSULTA_CRIADA = "notificacoes.consulta.criada.dlq";
 
     @Bean
     public Queue queueConsultaCriada () {
-        return new Queue(QUEUE_CONSULTA_CRIADA, true);
+        return QueueBuilder
+                .durable(QUEUE_CONSULTA_CRIADA)
+                .withArgument("x-dead-letter-exchange", DLX_NOTIFICACOES)
+                .build();
+    }
+
+    @Bean
+    public DirectExchange dlxExchange () {
+        return new DirectExchange(DLX_NOTIFICACOES);
+    }
+
+    @Bean
+    public Queue dlqQueue () {
+        return QueueBuilder
+                .durable(DLQ_CONSULTA_CRIADA)
+                .build();
+    }
+
+    @Bean
+    public Binding dlqBinding () {
+        return BindingBuilder.bind(dlqQueue()).to(dlxExchange()).with("");
     }
 
     @Bean
@@ -23,5 +47,14 @@ public class RabbitMQConfig {
         Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
         converter.setTypePrecedence(Jackson2JavaTypeMapper.TypePrecedence.INFERRED);
         return converter;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory manualAckContainerFactory (ConnectionFactory connectionFactory, MessageConverter jsonMessageConverter) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        return factory;
     }
 }
--- Servico_Notificacoes/src/main/java/com/HealthSys/Servico_Notificacoes/messaging/listener/NotificacaoListener.java
+++ Servico_Notificacoes/src/main/java/com/HealthSys/Servico_Notificacoes/messaging/listener/NotificacaoListener.java
@@ -1,18 +1,32 @@
 package com.HealthSys.Servico_Notificacoes.messaging.listener;
 
 import com.HealthSys.Servico_Notificacoes.messaging.event.ConsultaCriadaEvent;
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @Slf4j
 public class NotificacaoListener {
 
-    @RabbitListener(queues = "notificacoes.consulta.criada")
-    public void receberConsultaCriada (ConsultaCriadaEvent event) {
-        log.info("Notificação: nova consulta registrada - paciente {} com {} em {}", event.idPaciente(), event.nomeMedico(), event.dataConsulta());
-        // aqui, no futuro, entraria o envio de verdade (email, SMS, push...)
-        // por agora, o log já prova que o consumidor está funcionando
+    @RabbitListener(queues = "notificacoes.consulta.criada", containerFactory = "manualAckContainerFactory")
+    public void receberConsultaCriada (ConsultaCriadaEvent event, Channel channel, @Header (AmqpHeaders.DELIVERY_TAG) long deliveryTag) throws IOException {
+        try {
+            log.info("Notificação: nova consulta registrada - paciente {} com {} em {}", event.idPaciente(), event.nomeMedico(), event.dataConsulta());
+            // aqui, no futuro, entraria o envio de verdade (email, SMS, push...)
+            // por agora, o log já prova que o consumidor está funcionando
+
+            channel.basicAck(deliveryTag, false);
+        } catch (Exception e) {
+            log.error("Falha ao processar notificação, enviando para DLQ: {}", e.getMessage());
+            channel.basicNack(deliveryTag, false, false);
+        }
+
+
     }
 }
